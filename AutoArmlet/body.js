171e384941d160925f1e14c646b85269f6312e72
ivanius51 <ivanius51@gmail.com>
2016-07-16 11:14:31 +0300

    fist upload

---
+++ AutoArmlet/body.js
@@ -0,0 +1,66 @@
+﻿//Ivanius51 13.07.2016 АвтоАрмлет
+//Распространяется под лицензией "GNU General Public License" https://jxself.org/translations/gpl-2.ru.shtml
+
+//интервал(в секундах) через который будет делаться проверка меньше некуда
+var interval = 0.1
+//Глобальная переменная для проверки изменения НР
+var OldHP = 0
+
+
+function AutoArmletFunc()
+{
+	var User = Players.GetPlayerHeroEntityIndex(Game.GetLocalPlayerID())
+	var UserBuffs = Game.GetBuffsNames(User)
+	var ArmletIt = Game.GetAbilityByName(User,'item_armlet')
+	
+	if ((Game.IntersecArrays(UserBuffs,["modifier_item_armlet"]))&&(!Game.IntersecArrays(UserBuffs,["modifier_item_armlet_unholy_strength"]))&&(Entities.GetHealth(User)<=50)&&(Math.abs(OldHP-Entities.GetHealth(User))>400))
+		Game.ToggleAbil(User,ArmletIt,1);
+	//Предел НР после которого выключать армлет
+	if ((Game.IntersecArrays(UserBuffs,["modifier_item_armlet_unholy_strength"]))&&(Entities.GetHealth(User)<=220)&&(OldHP>Entities.GetHealth(User)))
+		Game.ToggleAbil(User,ArmletIt,1)
+	
+	OldHP = Entities.GetHealth(User)
+}
+
+var AutoArmletOnCheckBoxClick = function()
+{
+	if ( !AutoArmlet.checked ){
+		Game.ScriptLogMsg('Script disabled: AutoArmlet', '#ff0000')
+		return
+	}
+	//Регистрируем обычное переключение армлета
+	try
+	{
+		Game.AddCommand('__AutoArmlet', 
+		function()
+		{
+			var User = Players.GetPlayerHeroEntityIndex(Game.GetLocalPlayerID())
+			var UserBuffs = Game.GetBuffsNames(User)
+			var ArmletIt = Game.GetAbilityByName(User,'item_armlet')
+			if (Game.IntersecArrays(UserBuffs,["modifier_item_armlet_unholy_strength"]))
+				Game.ToggleAbil(User,ArmletIt,1)
+			
+			$.Schedule(0.005,function()
+			{
+				var User = Players.GetPlayerHeroEntityIndex(Game.GetLocalPlayerID());
+				var ArmletIt = Game.GetAbilityByName(User,'item_armlet');
+				Game.ToggleAbil(User,ArmletIt,1);
+			},0)
+		}, 'AutoArmlet Abuse',0)
+	}catch(e){}
+	
+	//циклически замкнутый таймер с проверкой условия с интервалом 'interval'
+	function maincheck(){ $.Schedule( interval,function(){
+		AutoArmletFunc()
+		if(AutoArmlet.checked)
+			maincheck()
+	})}
+	maincheck()
+	Game.ScriptLogMsg('Script enabled: AutoArmlet', '#00ff00')
+}
+
+//шаблонное добавление чекбокса в панель
+var Temp = $.CreatePanel( "Panel", $('#scripts'), "AutoArmlet" )
+Temp.SetPanelEvent( 'onactivate', AutoArmletOnCheckBoxClick )
+Temp.BLoadLayoutFromString( '<root><styles><include src="s2r://panorama/styles/dotastyles.vcss_c" /><include src="s2r://panorama/styles/magadan.vcss_c" /></styles><Panel><ToggleButton class="CheckBox" id="AutoArmlet" text="AutoArmlet"/></Panel></root>', false, false)  
+var AutoArmlet = $.GetContextPanel().FindChildTraverse( 'AutoArmlet' ).Children()[0]
---
+++ skillalert/body.js
@@ -0,0 +1,90 @@
+﻿//Показывает начало каста скилов
+//Пока что 100% показывает следующие скилы: стан лины, санстрайк, стан лешрака, торрент кунки
+//Edited 11.07.2016 Ivanius51 - Добавил  разгон бары, шар тускара и добавил проверку своих дебафов
+
+var modifiers = [
+	["modifier_invoker_sun_strike", 1.7],
+	["modifier_kunkka_torrent_thinker", 1.6],
+	["modifier_lina_light_strike_array", 0.5],
+	["modifier_leshrac_split_earth_thinker", 0.35],
+	["modifier_spirit_breaker_charge_of_darkness_vision", 1.5],
+	["modifier_tusk_snowball_visible", 1.5]
+]
+
+var z = []
+
+function SAllertEvery(){
+	try{if (!SkillAlert.checked){return}}catch(e){}
+	thinkers = Entities.GetAllEntitiesByName('npc_dota_thinker')
+	
+	var User = Players.GetPlayerHeroEntityIndex(Game.GetLocalPlayerID())
+
+	for ( var m in thinkers){
+		var ent = thinkers[m]
+		var xyz = Entities.GetAbsOrigin(ent)
+		var buffsnames = Game.GetBuffsNames(ent)
+		if(buffsnames.length!=1)
+			continue
+		var f = false
+		for(i in modifiers)
+		{
+			Game.ScriptLogMsg( buffsnames[0], '#00ff00')
+			if(modifiers[i][0]==buffsnames[0])
+				CreateTimerParticle(xyz,modifiers[i][1])	
+		}
+	}
+	
+	var UserBuffs = Game.GetBuffsNames(User)
+	var xyz = Entities.GetAbsOrigin(User)
+	
+	for (ibuff in UserBuffs)
+		for(imod in modifiers)
+		{
+			//Game.ScriptLogMsg( 'My buffs: ' + UserBuffs[ibuff], '#00ff00')
+			if (modifiers[imod][0]==UserBuffs[ibuff])
+			{
+				if ((modifiers[imod][0]=="modifier_spirit_breaker_charge_of_darkness_vision")||(modifiers[imod][0]=="modifier_tusk_snowball_visible"))
+					CreateFollowParticle(modifiers[imod][1],"particles/units/heroes/hero_spirit_breaker/spirit_breaker_charge_target_mark.vpcf",User)
+				else
+					CreateTimerParticle(xyz,modifiers[imod][1])
+			}
+		}
+}
+
+function CreateFollowParticle(time,particlepath,someobj,ent){
+	if(z.indexOf(ent)!=-1)
+		return
+	var p = Particles.CreateParticle(particlepath, ParticleAttachment_t.PATTACH_OVERHEAD_FOLLOW, someobj)
+	Particles.SetParticleControl(p, 0,  0)
+	z.push(ent)
+	$.Schedule(time+0.1,function(){ Particles.DestroyParticleEffect(p,p); z.splice(z.indexOf(ent),1); })
+}
+
+function CreateTimerParticle(xyz,time,ent){
+	if(z.indexOf(ent)!=-1)
+		return
+	var p = Particles.CreateParticle("particles/neutral_fx/roshan_spawn.vpcf", ParticleAttachment_t.PATTACH_ABSORIGIN, 0)
+	Particles.SetParticleControl(p, 0, xyz)
+	z.push(ent)
+	$.Schedule(time+0.1,function(){ Particles.DestroyParticleEffect(p,p); z.splice(z.indexOf(ent),1); })
+}
+
+function SkillAlertChkBox(){
+	if ( !SkillAlert.checked ){
+		Game.ScriptLogMsg('Script disabled: SkillAlert', '#ff0000')
+	}else{
+		function f(){ $.Schedule( 0.1,function(){
+			try{if (SkillAlert.checked){
+				SAllertEvery()
+				f()
+			}}catch(e){}
+		})}
+		f()
+		Game.ScriptLogMsg('Script enabled: SkillAlert', '#00ff00')
+	}
+}
+
+var Temp = $.CreatePanel( "Panel", $('#scripts'), "SkillAlert" )
+Temp.SetPanelEvent( 'onactivate', SkillAlertChkBox )
+Temp.BLoadLayoutFromString( '<root><styles><include src="s2r://panorama/styles/dotastyles.vcss_c" /><include src="s2r://panorama/styles/magadan.vcss_c" /></styles><Panel><ToggleButton class="CheckBox" id="SkillAlert" text="SkillAlert"/></Panel></root>', false, false)  
+var SkillAlert = $.GetContextPanel().FindChildTraverse( 'SkillAlert' ).Children()[0]
